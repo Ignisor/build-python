76811a0a7d798ed62aa6cd3bf7bcfa4e22eb5b3c
Ignisor <Ignis2497@gmail.com>
2019-04-02 16:00:01 +0300

    Added openssl to dependencies installation

--- build_python.sh
+++ build_python.sh
@@ -5,7 +5,7 @@ python_url=$1
 
 echo "Installing dependencies"
 sudo apt install -y build-essential checkinstall
-sudo apt install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+sudo apt install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev openssl
 
 echo "Getting sources to /tmp/python_source.tar.xz"
 wget $python_url -O /tmp/python_source.tar.xz
